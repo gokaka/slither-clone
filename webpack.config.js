21c814f70796794dea42cab39a463d364014d581
WU Kejia(JP) <kejia.wu@monstar-lab.com>
2017-10-16 12:55:10 +0900

    add copy webpack plugin, copy assets to dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 var path = require('path')
 var webpack = require('webpack')
 var BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
 
 // Phaser webpack config
 var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/')
@@ -37,7 +38,12 @@ module.exports = {
       server: {
         baseDir: ['./', './build']
       }
-    })
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: './assets/**/*', to: './'
+      }
+    ])
   ],
   module: {
     rules: [
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
 
 // Phaser webpack config
 var phaserModule = path.join(__dirname, '/node_modules/phaser-ce/')
@@ -35,7 +36,12 @@ module.exports = {
         comments: false
       }
     }),
-    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'/* chunkName= */, filename: 'vendor.bundle.js'/* filename= */})
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor'/* chunkName= */, filename: 'vendor.bundle.js'/* filename= */}),
+    new CopyWebpackPlugin([
+      {
+        from: './assets/**/*', to: './'
+      }
+    ])
   ],
   module: {
     rules: [
